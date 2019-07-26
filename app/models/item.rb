4ec3c1ccff2cdbecfd53d77dbb1ab74ab134a8e4
Yousri <laifayousri@gmail.com>
2019-07-26 19:19:13 +0200

    Item migration was run

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(, )
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Item < ApplicationRecord
   validates :title, presence: true, length: { in: 4..140 }
   validates :description, presence: true, length: { in: 20..1000 }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(, )
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryBot.define do
   factory :item do
     title { "MyString" }
