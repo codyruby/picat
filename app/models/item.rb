a8c49962b30396dbfbf242a8125596e6fc0eb196
Yousri <laifayousri@gmail.com>
2019-07-26 19:08:41 +0200

    add validates to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  validates :title, presence: true, length: { in: 4..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price,
+            presence: true,
+            numericality: {
+              greater_than_or_equal_to: 0
+            }
+  validates :image_url, presence: true
 end
