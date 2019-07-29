a58cfab95e31f405282c65e16e6f00e83845bb42
Yousri <laifayousri@gmail.com>
2019-07-29 15:19:48 +0200

    Edit test in models/item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,33 +18,33 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  before do
-    @item = FactoryBot.create(:item)
+  let(:item) do
+    FactoryBot.create(:item)
   end
 
   it "has a valid factory" do
     expect(build(:item)).to be_valid
   end
 
-  context "validation" do
+  context "when validation" do
     it "is valid with valid attributes" do
-      expect(@item).to be_a(described_class)
+      expect(item).to be_a(described_class)
     end
 
     describe "#title" do
-      it { expect(@item).to validate_presence_of(:title) }
+      it { expect(item).to validate_presence_of(:title) }
     end
 
     describe "#description" do
-      it { expect(@item).to validate_presence_of(:description) }
+      it { expect(item).to validate_presence_of(:description) }
     end
 
     describe "#price" do
-      it { expect(@item).to validate_length_of(:price) }
+      it { expect(item).to validate_length_of(:price) }
     end
 
     describe "#image_url" do
-      it { expect(@item).to validate_length_of(:image_url) }
+      it { expect(item).to validate_length_of(:image_url) }
     end
   end
 end
