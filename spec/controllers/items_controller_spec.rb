149021b1a315c92e46e681288a0b02a46e8798db
Yousri <laifayousri@gmail.com>
2019-07-31 16:38:27 +0200

    Edit test to items_controller_spec.rb

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,17 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
+  describe "GET index" do
+    it "assigns @items" do
+      # création d'une instance
+      item = FactoryBot.create(:item)
+
+      # on va sur index
       get :index
-      expect(response).to have_http_status(:success)
+
+      # @items doit être une array qui contient item
+      expect(assigns(:items)).to eq([item])
+    end
+
+    it "renders the index template" do
+      # va sur index
+      get :index
+
+      # on doit rediriger vers index
+      expect(response).to render_template("index")
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe "GET show" do
+    subject { get :show, params: { id: resource.id } }
+
+    let(:resource){ FactoryBot.create(:item) }
+
+    it "assigns @items" do
+      # on va sur show
+      subject
+      expect(assigns(:item)).to eq(resource)
+    end
+
+    it "renders the show template" do
+      # va sur show
+
+      # on doit rediriger vers show
+      expect(subject).to render_template("show")
     end
   end
 end
