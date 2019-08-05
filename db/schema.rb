edb27e16cd7b71d169c2b1a5ff2731b02b363968
Yousri <laifayousri@gmail.com>
2019-08-05 20:20:15 +0200

    Run migration join table line_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_165108) do
+ActiveRecord::Schema.define(version: 2019_08_05_172512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2019_08_05_165108) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "quantity", default: 1
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["item_id"], name: "index_line_items_on_item_id"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :bigint           not null, primary key
+#  quantity   :integer          default(1)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  cart_id    :bigint
+#  item_id    :bigint
+#
+# Indexes
+#
+#  index_line_items_on_cart_id  (cart_id)
+#  index_line_items_on_item_id  (item_id)
+#
+
 FactoryBot.define do
   factory :line_item do
     quantity { 1 }
