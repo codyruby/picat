10142cd776535b6d8c77eed2489031344dab2847
Yousri <laifayousri@gmail.com>
2019-08-05 19:11:22 +0200

    Add relation between user and cart models

--- db/migrate/20190805165108_create_carts.rb
+++ db/migrate/20190805165108_create_carts.rb
@@ -2,6 +2,10 @@
 
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
-    create_table :carts, &:timestamps
+    create_table :carts do |t|
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
   end
 end
