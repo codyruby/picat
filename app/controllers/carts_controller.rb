4504e75f473c5853dc1e6c9eff516e8f31fd67b7
Yousri <laifayousri@gmail.com>
2019-08-06 17:31:39 +0200

    Add def create and def cart_params

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,16 @@
 
 class CartsController < ApplicationController
   def show; end
+
+  def create
+    @cart = Cart.new(cart_params)
+    @cart.user_id = current_user.id
+    @cart.save
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:user_id)
+  end
 end
