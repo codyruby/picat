24692a7ffa9118046e5eaf17d4ebb55be32e6e08
Yousri <laifayousri@gmail.com>
2019-08-06 15:21:37 +0200

    Controller line_items created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'carts/:id', to: "carts#show", as: "cart"
 
   resources :items, only: [:index, :show]
+  resources :line_items, only: [:create]
 
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
