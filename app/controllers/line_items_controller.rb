cbdda191786a37969f837d77d7c18cfa258160e8
Yousri <laifayousri@gmail.com>
2019-08-07 18:33:06 +0200

    Edit def create to line_items_controller.rb

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class LineItemsController < ApplicationController
-  def create; end
+  def create
+    @item = Item.find(params[:item_id])
+    @cart = Cart.find_by(user_id: current_user.id)
+    @line_item = @cart.line_items.create(item_id: params[:item_id])
+
+    redirect_to cart_path(@cart.id)
+  end
 end
