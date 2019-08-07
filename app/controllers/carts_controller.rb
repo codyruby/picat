fd26df3b3f7d656520e5eac70886ded8bfadb8aa
Yousri <laifayousri@gmail.com>
2019-08-07 15:02:13 +0200

    Edit carts_controller.rb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,16 +2,4 @@
 
 class CartsController < ApplicationController
   def show; end
-
-  def create
-    @cart = Cart.new(cart_params)
-    @cart.user_id = current_user.id
-    @cart.save
-  end
-
-  private
-
-  def cart_params
-    params.require(:cart).permit(:user_id)
-  end
 end
