80fb867b133b6c780a6396aa50f9025da2becf84
Yousri <laifayousri@gmail.com>
2019-08-09 17:13:37 +0200

    remove def create in line_items_controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class LineItemsController < ApplicationController
-  def create
-    @item = Item.find(params[:item_id])
-    @cart = Cart.find_by(user_id: current_user.id)
-    @line_item = @cart.line_items.create(item_id: params[:item_id])
-
-    redirect_to cart_path(@cart.id)
-  end
+  def create; end
 end
