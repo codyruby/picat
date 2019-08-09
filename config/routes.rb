52f52d0ae00fd5bca7af842e5f4b6ad75d3e28ac
Yousri <laifayousri@gmail.com>
2019-08-09 17:09:36 +0200

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/home', to: 'statics#home'
-  get 'carts/:id', to: "carts#show", as: "cart"
 
   resources :items, only: [:index, :show]
-  resources :line_items, only: [:create]
+  resources :carts, only: [:show, :update]
 
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
