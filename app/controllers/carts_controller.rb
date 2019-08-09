a2925fe015164de3786adaba7e93c2bc26bb5a2d
Yousri <laifayousri@gmail.com>
2019-08-09 17:14:56 +0200

    add def update to carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @cart = Cart.find(params[:id])
+  end
+
+  def update
+    @cart = Cart.find_by(user_id: current_user)
+    @item = Item.find(params[:id])
+    @cart.items << @item
+
+    redirect_to cart_path(@cart)
   end
 end
