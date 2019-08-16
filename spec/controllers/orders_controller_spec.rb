a38a3c0a15aa2b44426bddc41d8a30a52cad3592
Yousri <laifayousri@gmail.com>
2019-08-16 14:05:59 +0200

    Edit test to controllers/orders_controller_spec.rb

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -3,24 +3,60 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:order) { user.orders }
+  let!(:item) { create(:item) }
+  let!(:cart) { user.cart }
+
   describe "GET #index" do
-    it "returns http success" do
+    it "gets index" do
       get :index
-      expect(response).to have_http_status(:success)
+      sign_in user
+      response.should be_success
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe "GET show" do
+    subject { get :show, params: { id: order.ids } }
+
+    it "renders the show template" do
+      expect(subject).to render_template("show")
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    context "with valid attributes" do
+      it "creates a new order" do
+        sign_in user
+        post :create, params: { user_id: user, amount: 9.99 }
+
+        expect(Order.count).to eq(1)
+      end
+
+      it "redirects to after new order" do
+        sign_in user
+        post :create, params: { user_id: user, amount: 9.99 }
+
+        response.should redirect_to root_path
+      end
     end
+
+    # context "with invalid attributes" do
+    #   it "doest not create a new order" do
+    #     sign_in user
+    #     cart = Cart.create(user_id: user)
+    #     expect {
+    #       post :create, params: { }
+
+    #     }.to_not change(Order, :count)
+    #   end
+
+    #   it "redirect_to cart_path after not created" do
+    #     sign_in user
+    #     cart = Cart.create(user_id: user)
+    #     post :create, params: { user_id: "invalid" }
+    #     expect(cart_path(user.cart)).to redirect_to cart_path(user.cart)
+    #   end
+    #  end
   end
 end
