4a2b77b98d540bc8faefc986b5884c9599a7f683
Yousri <laifayousri@gmail.com>
2019-08-19 13:45:47 +0200

    Add def order_confirmation to user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@monsite.fr'
+
+  def order_confirmation(order)
+    @order = order
+    @items = @order.items
+    @url = 'http://monsite.fr/login'
+
+    mail(to: @order.user.email, subject: 'Picat order confirmation !')
+  end
 end
