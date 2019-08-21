a32a7e31a438651fd0f8c2eff084c38e361c91d9
Yousri <laifayousri@gmail.com>
2019-08-21 17:04:46 +0200

    Edit user_mailer.rb add def order_confirmation_to_admin

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,12 @@ class UserMailer < ApplicationMailer
 
     mail(to: @order.user.email, subject: 'Picat order confirmation !')
   end
+
+  def order_confirmation_to_admin(order)
+    @order = order
+    @line_items = @order.user.cart.line_items
+    @url = 'http://monsite.fr/login'
+
+    mail(to: 'vittelform91@gmail.com', subject: 'Picat order confirmation !')
+  end
 end
