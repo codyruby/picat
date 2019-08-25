2700133a8b220e2a2cc5bea7650fda22a13468d5
Yousri <laifayousri@gmail.com>
2019-08-25 18:39:44 +0200

    Edit items_controller CRUD completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,38 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+
+    redirect_to item_path(@item)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :picture)
+  end
 end
